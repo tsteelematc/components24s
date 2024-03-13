607921249ae0cefaa462c4eb9aeaef9d9e89a9b9
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-03-13 08:53:31 -0500

    Fixed incorrect app.module.ts configuration
    Removed components from imports and added to declarations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-
-import { JtaDropdownComponent } from './jta-dropdown/jta-dropdown.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 import { TjsMultiplicationComponent } from './tjs-multiplication/tjs-multiplication.component';
+import { JtaDropdownComponent } from './jta-dropdown/jta-dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaToppingsComponent,
+    TjsMultiplicationComponent,
     JtaDropdownComponent
   ],
   imports: [
@@ -34,9 +33,7 @@ import { TjsMultiplicationComponent } from './tjs-multiplication/tjs-multiplicat
     ReactiveFormsModule,
     MatInputModule,
     MatSelectModule,
-    MatFormFieldModule,
-    PizzaToppingsComponent,
-    TjsMultiplicationComponent
+    MatFormFieldModule
   ],
   providers: [
     provideAnimationsAsync()
